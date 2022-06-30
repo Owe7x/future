56451abadeb844fcaba09815ac9bbe8b75e2fe52
Timur <thedickst@gmail.com>
2022-06-30 17:59:00 +0300

    new fix 3

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,3 +2,5 @@ $('.checkbox').click(() => {
     $('.menu-items').toggleClass('menu-items-active')
     $('.page').toggleClass('page-active')
 })
+
+$('.page-thanks').css('height' , window.innerHeight);
