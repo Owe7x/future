25924b08cbf5ff840effb7a99b21b5a3639fb202
Timur <thedickst@gmail.com>
2022-07-01 12:55:05 +0300

    new fix 3

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,3 +4,20 @@ $('.checkbox').click(() => {
 })
 
 $('.page-thanks').css('height' , window.innerHeight);
+
+$('.slider-mb-fest').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });;
+
+$('.slider-mb-for').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });;
+  $('.slider-mb-event').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });;
